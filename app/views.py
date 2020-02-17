0d8da0eec5b131daa2ca644406349a3a32c56779
Rajheem O'Connor <rajheem1@gmail.com>
2020-02-16 22:23:52 -0500

    'Lab3'

--- app/views.py
+++ app/views.py
@@ -7,6 +7,22 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField
+from wtforms.validators import DataRequired,Email
+
+
+
+from app import mail
+from flask_mail import Message
+class ContactForm(FlaskForm):
+    name = StringField('Name',validators=[DataRequired()])
+    email = StringField('Email',validators=[Email(),DataRequired()])
+    subject=StringField('Subject',validators=[DataRequired()])
+    message = TextAreaField('Message',validators=[DataRequired()])
+
+
+
 
 
 ###
@@ -23,6 +39,18 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact',methods=['GET','POST'])
+def contact():
+    form=ContactForm()
+    if form.validate_on_submit():
+        msg = Message(request.form['subject'],sender=(request.form['name'],
+        "from@example.com"),recipients=[request.form['email']])
+        msg.body = request.form['message']
+        mail.send(msg) 
+        return redirect(url_for('home'))
+    return render_template('contact.html',form=form)
+    
 
 
 ###
