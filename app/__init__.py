ff1ae38a05778cac42a580e7ebed80577c4e639a
Rajheem O'Connor <rajheem1@gmail.com>
2020-02-20 18:58:39 -0500

    'Lab3'

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'enter some random passphrase'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '465' # (or try 2525)
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = '497468404d6669'
+app.config['MAIL_PASSWORD'] = '4d29955f3b406c'
 mail = Mail(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -10,16 +10,13 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField
 from wtforms.validators import DataRequired,Email
+from app.forms import ContactForm
 
 
 
 from app import mail
 from flask_mail import Message
-class ContactForm(FlaskForm):
-    name = StringField('Name',validators=[DataRequired()])
-    email = StringField('Email',validators=[Email(),DataRequired()])
-    subject=StringField('Subject',validators=[DataRequired()])
-    message = TextAreaField('Message',validators=[DataRequired()])
+
 
 
 
